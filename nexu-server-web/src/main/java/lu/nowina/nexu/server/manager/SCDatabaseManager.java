586101139184db703b03c63f4b8723cc2f522ad5
david.naramski <david.naramski@nowina.lu>
2015-10-21 12:02:53 +0200

    Test for SCDataBaseManager

--- nexu-server-web/src/main/java/lu/nowina/nexu/server/manager/SCDatabaseManager.java
+++ nexu-server-web/src/main/java/lu/nowina/nexu/server/manager/SCDatabaseManager.java
@@ -1,19 +1,37 @@
+/**
+ * © Nowina Solutions, 2015-2015
+ *
+ * Concédée sous licence EUPL, version 1.1 ou – dès leur approbation par la Commission européenne - versions ultérieures de l’EUPL (la «Licence»).
+ * Vous ne pouvez utiliser la présente œuvre que conformément à la Licence.
+ * Vous pouvez obtenir une copie de la Licence à l’adresse suivante:
+ *
+ * http://ec.europa.eu/idabc/eupl5
+ *
+ * Sauf obligation légale ou contractuelle écrite, le logiciel distribué sous la Licence est distribué «en l’état»,
+ * SANS GARANTIES OU CONDITIONS QUELLES QU’ELLES SOIENT, expresses ou implicites.
+ * Consultez la Licence pour les autorisations et les restrictions linguistiques spécifiques relevant de la Licence.
+ */
 package lu.nowina.nexu.server.manager;
 
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.InputStream;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SCDatabaseManager {
+	
+	private static final Logger logger = Logger.getLogger(SCDatabaseManager.class.getName());
 
 	@Value("${nexuDatabase}")
-	private File nexuDatabaseFile;
+	Resource nexuDatabaseFile;
 
 	private byte[] data;
 
@@ -26,10 +44,11 @@ public class SCDatabaseManager {
 		}
 
 		if (data == null) {
-			try (FileInputStream in = new FileInputStream(nexuDatabaseFile)) {
+			try (InputStream in = nexuDatabaseFile.getInputStream()) {
 				data = IOUtils.toByteArray(in);
 			} catch (Exception e) {
-				throw new RuntimeException();
+				logger.log(Level.SEVERE, "Cannot read file " + nexuDatabaseFile, e);
+				throw new RuntimeException(e);
 			}
 		}
 
@@ -42,7 +61,7 @@ public class SCDatabaseManager {
 			try {
 				MessageDigest digest = MessageDigest.getInstance("MD5");
 				databaseDigest = Hex.encodeHexString(digest.digest(getData()));
-			} catch (Exception e) {
+			} catch (NoSuchAlgorithmException e) {
 				throw new RuntimeException(e);
 			}
 		}
