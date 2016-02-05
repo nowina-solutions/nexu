da8d61853ca12a6984b6f570f11dd21382d2f43c
jean.lepropre <jean.lepropre@nowina.lu>
2016-02-05 15:05:41 +0100

    NOW-33

--- nexu-proxy/src/main/java/lu/nowina/nexu/server/config/WebConfig.java
+++ nexu-proxy/src/main/java/lu/nowina/nexu/server/config/WebConfig.java
@@ -14,11 +14,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.ws.config.annotation.EnableWs;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @Configuration
 @ComponentScan(basePackages = { "lu.nowina.nexu.server" })
+@EnableWebMvc
 @EnableWs
 @ImportResource({ "classpath:META-INF/cxf/cxf.xml", "classpath:META-INF/cxf/cxf-servlet.xml" })
 public class WebConfig extends WebMvcConfigurerAdapter {
@@ -35,6 +40,29 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		return messageSource;
 	}
 
+	@Bean
+	public ServletContextTemplateResolver defaultTemplateResolver() {
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+		templateResolver.setPrefix("/WEB-INF/html/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setCacheable(false);
+		return templateResolver;
+	}
+
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(defaultTemplateResolver());
+		return templateEngine;
+	}
+
+	@Bean
+	public ThymeleafViewResolver viewResolver() {
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(templateEngine());
+		return viewResolver;
+	}
+	
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
---
+++ nexu-proxy/src/main/java/lu/nowina/nexu/server/web/controller/PageController.java
@@ -0,0 +1,14 @@
+package lu.nowina.nexu.server.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "index";
+	}
+	
+}
