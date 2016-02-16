82dd919ad320ee4089aa3c5cba352a1d605d7166
Axel Abinet <axel.abinet@nowina.lu>
2016-02-16 16:23:33 +0100

    NEXU-28

--- nexu-core/src/main/java/lu/nowina/nexu/view/ui/KeySelectionController.java
+++ nexu-core/src/main/java/lu/nowina/nexu/view/ui/KeySelectionController.java
@@ -14,23 +14,32 @@
 package lu.nowina.nexu.view.ui;
 
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.MouseButton;
 import lu.nowina.nexu.view.core.AbstractUIOperationController;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import eu.europa.esig.dss.token.DSSPrivateKeyEntry;
+import eu.europa.esig.dss.x509.CertificateToken;
 
 public class KeySelectionController extends AbstractUIOperationController<DSSPrivateKeyEntry> implements Initializable {
 
@@ -44,6 +53,18 @@ public class KeySelectionController extends AbstractUIOperationController<DSSPri
 
 	@FXML
 	private ListView<DSSPrivateKeyEntry> listView;
+	
+	@FXML
+	private TextArea taX500Principal;
+	
+	@FXML
+	private Label startDate;
+	
+	@FXML
+	private Label endDate;
+	
+	@FXML
+	private Label usage;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -75,6 +96,58 @@ public class KeySelectionController extends AbstractUIOperationController<DSSPri
 
 			return cell;
 		});
+		
+		listView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				CertificateToken token = listView.getSelectionModel().getSelectedItem().getCertificate();
+				taX500Principal.setText(token.getSubjectX500Principal().toString().replace(", ", "\n"));
+				
+				SimpleDateFormat format = new SimpleDateFormat("dd MMMMMM yyyy", Locale.ENGLISH);
+				startDate.setText(format.format(token.getNotBefore()));
+				endDate.setText(format.format(token.getNotAfter()));
+				usage.setText(createKeyUsageString(token));
+			}
+			
+		});
+		
+		taX500Principal.setEditable(false);
+		taX500Principal.setMouseTransparent(true);
+		taX500Principal.setFocusTraversable(false);
+	}
+	
+	private String createKeyUsageString(CertificateToken token) {
+		StringBuilder builder = new StringBuilder();
+		boolean[] keyUsages = token.getCertificate().getKeyUsage();
+		if(keyUsages[0]) {
+			builder.append("Digital Signature\n");
+		}
+		if(keyUsages[1]) {
+			builder.append("Non Repudiation\n");
+		}
+		if(keyUsages[2]) {
+			builder.append("Key Encipherment\n");
+		}
+		if(keyUsages[3]) {
+			builder.append("Data Encipherment\n");
+		}
+		if(keyUsages[4]) {
+			builder.append("Key Agreement\n");
+		}
+		if(keyUsages[5]) {
+			builder.append("Key Certificate Signature\n");
+		}
+		if(keyUsages[6]) {
+			builder.append("CRL Signing\n");
+		}
+		if(keyUsages[7]) {
+			builder.append("Encipher Only\n");
+		}
+		if(keyUsages[8]) {
+			builder.append("Decipher Only\n");
+		}
+		return builder.toString();
 	}
 
 	@Override
