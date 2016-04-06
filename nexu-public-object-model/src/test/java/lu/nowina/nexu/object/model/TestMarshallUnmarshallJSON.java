3d0f026d1f2460fa6dc8079d48ed8b0c1c2e3b2a
jean.lepropre <jean.lepropre@nowina.lu>
2016-04-06 16:44:29 +0200

    NEXU-49

--- nexu-public-object-model/src/test/java/lu/nowina/nexu/object/model/TestMarshallUnmarshallJSON.java
+++ nexu-public-object-model/src/test/java/lu/nowina/nexu/object/model/TestMarshallUnmarshallJSON.java
@@ -15,6 +15,13 @@ package lu.nowina.nexu.object.model;
 
 import org.junit.Test;
 
+import eu.europa.esig.dss.DigestAlgorithm;
+
+import java.nio.charset.StandardCharsets;
+
+import org.junit.Assert;
+import lu.nowina.nexu.json.GsonHelper;
+
 /**
  * JUnit test class for JSON marshalling/unmarshalling.
  *
@@ -26,9 +33,34 @@ public class TestMarshallUnmarshallJSON {
 		super();
 	}
 
+	private void setCommonRequestFields(final NexuRequest request) {
+		request.setExternalId("externalId");
+		request.setNonce("nonce");
+		request.setRequestSeal("requestSeal");
+		request.setUserLocale("userLocale");
+	}
+	
+	private void assertCommonRequestFields(final lu.nowina.nexu.api.NexuRequest request) {
+		Assert.assertEquals("externalId", request.getExternalId());
+		Assert.assertEquals("nonce", request.getNonce());
+		Assert.assertEquals("requestSeal", request.getRequestSeal());
+		Assert.assertEquals("userLocale", request.getUserLocale());
+	}
+	
 	@Test
 	public void testGetCertificateRequest() {
-		//TODO
+		final CertificateFilter certFilter = new CertificateFilter();
+		certFilter.setPurpose(Purpose.AUTHENTICATION);
+		final GetCertificateRequest getCertificateRequest = new GetCertificateRequest(certFilter);
+		setCommonRequestFields(getCertificateRequest);
+		final String json = GsonHelper.toJson(getCertificateRequest);
+		
+		final lu.nowina.nexu.api.GetCertificateRequest getCertificateRequestAPI = GsonHelper.fromJson(json, lu.nowina.nexu.api.GetCertificateRequest.class);
+		Assert.assertNotNull(getCertificateRequestAPI);
+		assertCommonRequestFields(getCertificateRequestAPI);
+		Assert.assertNotNull(getCertificateRequestAPI.getCertificateFilter());
+		Assert.assertNull(getCertificateRequestAPI.getCertificateFilter().getCertificateSHA1());
+		Assert.assertEquals(lu.nowina.nexu.api.Purpose.AUTHENTICATION, getCertificateRequestAPI.getCertificateFilter().getPurpose());
 	}
 
 	@Test
@@ -38,7 +70,27 @@ public class TestMarshallUnmarshallJSON {
 
 	@Test
 	public void testSignatureRequest() {
-		//TODO
+		final TokenId tokenId = new TokenId();
+		tokenId.setId("tokenId");
+		final ToBeSigned tbs = new ToBeSigned();
+		tbs.setBytes("dG90bw==");
+		final SignatureRequest signatureRequest = new SignatureRequest();
+		setCommonRequestFields(signatureRequest);
+		signatureRequest.setDigestAlgorithm("SHA1");
+		signatureRequest.setKeyId("keyId");
+		signatureRequest.setToBeSigned(tbs);
+		signatureRequest.setTokenId(tokenId);
+		final String json = GsonHelper.toJson(signatureRequest);
+
+		final lu.nowina.nexu.api.SignatureRequest signatureRequestAPI = GsonHelper.fromJson(json, lu.nowina.nexu.api.SignatureRequest.class);
+		Assert.assertNotNull(signatureRequestAPI);
+		assertCommonRequestFields(signatureRequestAPI);
+		Assert.assertEquals(DigestAlgorithm.SHA1, signatureRequestAPI.getDigestAlgorithm());
+		Assert.assertEquals("keyId", signatureRequestAPI.getKeyId());
+		Assert.assertNotNull(signatureRequestAPI.getToBeSigned());
+		Assert.assertEquals("toto", new String(signatureRequestAPI.getToBeSigned().getBytes(), StandardCharsets.UTF_8));
+		Assert.assertNotNull(signatureRequestAPI.getTokenId());
+		Assert.assertEquals("tokenId", signatureRequestAPI.getTokenId().getId());
 	}
 
 	@Test
@@ -48,7 +100,13 @@ public class TestMarshallUnmarshallJSON {
 
 	@Test
 	public void testGetIdentityInfoRequest() {
-		//TODO
+		final GetIdentityInfoRequest getIdentityInfoRequest = new GetIdentityInfoRequest();
+		setCommonRequestFields(getIdentityInfoRequest);
+		final String json = GsonHelper.toJson(getIdentityInfoRequest);
+		
+		final lu.nowina.nexu.api.GetIdentityInfoRequest getIdentityInfoRequestAPI = GsonHelper.fromJson(json, lu.nowina.nexu.api.GetIdentityInfoRequest.class);
+		Assert.assertNotNull(getIdentityInfoRequestAPI);
+		assertCommonRequestFields(getIdentityInfoRequestAPI);
 	}
 
 	@Test
@@ -58,7 +116,18 @@ public class TestMarshallUnmarshallJSON {
 
 	@Test
 	public void testAuthenticateRequest() {
-		//TODO
+		final ToBeSigned tbs = new ToBeSigned();
+		tbs.setBytes("dG90bw==");
+		final AuthenticateRequest authenticateRequest = new AuthenticateRequest();
+		setCommonRequestFields(authenticateRequest);
+		authenticateRequest.setChallenge(tbs);
+		final String json = GsonHelper.toJson(authenticateRequest);
+		
+		final lu.nowina.nexu.api.AuthenticateRequest authenticateRequestAPI = GsonHelper.fromJson(json, lu.nowina.nexu.api.AuthenticateRequest.class);
+		Assert.assertNotNull(authenticateRequestAPI);
+		assertCommonRequestFields(authenticateRequestAPI);
+		Assert.assertNotNull(authenticateRequestAPI.getChallenge());
+		Assert.assertEquals("toto", new String(authenticateRequestAPI.getChallenge().getBytes(), StandardCharsets.UTF_8));
 	}
 
 	@Test
