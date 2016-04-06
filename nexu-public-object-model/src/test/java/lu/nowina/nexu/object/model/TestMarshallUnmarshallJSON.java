a8ba4d5870a5a5a150556024791e82acfe73b963
jean.lepropre <jean.lepropre@nowina.lu>
2016-04-06 12:30:45 +0200

    NEXU-49

---
+++ nexu-public-object-model/src/test/java/lu/nowina/nexu/object/model/TestMarshallUnmarshallJSON.java
@@ -0,0 +1,68 @@
+/**
+ * © Nowina Solutions, 2015-2016
+ *
+ * Concédée sous licence EUPL, version 1.1 ou – dès leur approbation par la Commission européenne - versions ultérieures de l’EUPL (la «Licence»).
+ * Vous ne pouvez utiliser la présente œuvre que conformément à la Licence.
+ * Vous pouvez obtenir une copie de la Licence à l’adresse suivante:
+ *
+ * http://ec.europa.eu/idabc/eupl5
+ *
+ * Sauf obligation légale ou contractuelle écrite, le logiciel distribué sous la Licence est distribué «en l’état»,
+ * SANS GARANTIES OU CONDITIONS QUELLES QU’ELLES SOIENT, expresses ou implicites.
+ * Consultez la Licence pour les autorisations et les restrictions linguistiques spécifiques relevant de la Licence.
+ */
+package lu.nowina.nexu.object.model;
+
+import org.junit.Test;
+
+/**
+ * JUnit test class for JSON marshalling/unmarshalling.
+ *
+ * @author Jean Lepropre (jean.lepropre@nowina.lu)
+ */
+public class TestMarshallUnmarshallJSON {
+
+	public TestMarshallUnmarshallJSON() {
+		super();
+	}
+
+	@Test
+	public void testGetCertificateRequest() {
+		//TODO
+	}
+
+	@Test
+	public void testGetCertificateResponse() {
+		//TODO
+	}
+
+	@Test
+	public void testSignatureRequest() {
+		//TODO
+	}
+
+	@Test
+	public void testSignatureResponse() {
+		//TODO
+	}
+
+	@Test
+	public void testGetIdentityInfoRequest() {
+		//TODO
+	}
+
+	@Test
+	public void testGetIdentityInfoResponse() {
+		//TODO
+	}
+
+	@Test
+	public void testAuthenticateRequest() {
+		//TODO
+	}
+
+	@Test
+	public void testAuthenticateResponse() {
+		//TODO
+	}
+}
