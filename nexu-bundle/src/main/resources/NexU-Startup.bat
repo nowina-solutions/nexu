6faf2c9cda6e2ffbacbafe15e06894903e1cc480
simon.ghisalberti <simon.ghisalberti@GHISALBERTI.NOWINA.LU>
2017-05-17 07:41:38 +0200

    NEXU-82 : bundle bat file

--- nexu-bundle/src/main/resources/NexU-Startup.bat
+++ nexu-bundle/src/main/resources/NexU-Startup.bat
@@ -8,11 +8,6 @@ rem http://ec.europa.eu/idabc/eupl5
 rem
 rem Sauf obligation légale ou contractuelle écrite, le logiciel distribué sous la Licence est distribué «en l’état»,
 rem SANS GARANTIES OU CONDITIONS QUELLES QU’ELLES SOIENT, expresses ou implicites.
-rem Consultez la Licence pour les autorisations et les restrictions linguistiques spécifiques relevant de la Licence.
 
 @echo off
-
-set JRE_HOME=.\java
-set CATALINA_HOME=.\tomcat
-
-start javaw -jar nexu.jar
+start .\java\bin\javaw -Djavafx.preloader=lu.nowina.nexu.NexUPreLoader -Dglass.accessible.force=false -jar nexu.jar
